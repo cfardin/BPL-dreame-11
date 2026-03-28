5514a764e1b790ccc72b61d0d2879171aaa00cf7
cfardin <cfardin51@gmail.com>
2026-03-28 12:31:28 +0600

    hero banner done

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import heroBG from "../../assets/bg-shadow.png";
+import heroLogo from "../../assets/banner-main.png";
+
+const Hero = () => {
+  return (
+    <div className="w-11/12 m-auto">
+      <div
+        className="hero h-136.25 bg-black rounded-3xl"
+        style={{
+          //   backgroundImage:
+          //     "url()",
+          backgroundImage: `url(${heroBG})`,
+        }}
+      >
+
+        <div className="hero-content text-neutral-content text-center flex flex-col">
+            {/* hero logo */}
+          <div>
+            <img src={heroLogo} alt="" />
+          </div>
+          <div className="">
+            <h1 className="mb-5 text-5xl font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+            <p className="mb-5">
+                Beyond Boundaries Beyond Limits
+            </p>
+            {/* <button className="btn btn-primary">Claim Free Credit</button> */}
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
